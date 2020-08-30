d11250a54af4c694ccd430d4981a63a8d90a869f
Francisco Ruiz Rivas <francisco@ruizrivas.net>
2020-08-30 17:34:45 -0300

    Completed Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album'
-import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {

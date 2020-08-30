a9791d04f70c78ee0509c7ad491a2416f86ae4b3
Francisco Ruiz Rivas <francisco@ruizrivas.net>
2020-08-30 17:30:38 -0300

    Completed Module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import 'rxjs'
-import { Album } from 'app/album';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
